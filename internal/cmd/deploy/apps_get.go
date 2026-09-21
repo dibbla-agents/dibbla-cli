03ff22bd0f1676490bd098bd8bfc1e3875e72671
erik knave <erik@dibbla.com>
2026-09-21 15:02:13 +0000

    Merge origin/main into story/dib-965 (shortSHA now lives in export.go)

--- internal/cmd/deploy/apps_get.go
+++ internal/cmd/deploy/apps_get.go
@@ -297,13 +297,6 @@ func agentLine(m *apps.SecurityMaintenance) string {
 	}
 }
 
-func shortSHA(sha string) string {
-	if len(sha) > 7 {
-		return sha[:7]
-	}
-	return sha
-}
-
 func orDash(s string) string {
 	if s == "" {
 		return "-"
